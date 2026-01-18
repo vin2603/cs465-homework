26b4bda97db810a5776cf167a4b63f9edc8f7a90
Vincent Le <vincentle@Vincents-MacBook-Air.local>
2026-01-18 13:43:09 -0800

    Homeowork 1 solution

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -9,8 +9,20 @@ Use console.log() to write the proper output to the command line.
 
 **/
 const fizzbuzz = () => {
-  //code here
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
 };
+
+fizzbuzz();
 
 // 1
 // 2
